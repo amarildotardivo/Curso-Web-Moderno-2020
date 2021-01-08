93d67788c9ea3b079aa1f6be9e830e020a732aa6
amarildotardivo <58271527+amarildotardivo@users.noreply.github.com>
2021-01-08 11:33:00 -0300

    Create IF1.js

---
+++ exercicios-js/controle/IF1.js
@@ -0,0 +1,31 @@
+function soBoaNoticia(nota) {
+    if ( nota >= 7 ) {
+        console.log('Aprovado com ' + nota)
+    }
+}
+
+soBoaNoticia(8.1)
+soBoaNoticia(6.1)
+
+function seForVerdadeEuFalo(valor) {
+    // JS automáticamente quando tem apenas um valor, entende que ele pode ser verdadeiro ou falso
+    if(valor) {
+        console.log('É verdade ... ' + valor)
+    }
+}
+
+//falso
+seForVerdadeEuFalo()
+seForVerdadeEuFalo(null)
+seForVerdadeEuFalo(undefined)
+seForVerdadeEuFalo(NaN)
+seForVerdadeEuFalo('')
+seForVerdadeEuFalo(0)
+
+//verdade
+seForVerdadeEuFalo(-1)
+seForVerdadeEuFalo(' ')
+seForVerdadeEuFalo('?')
+seForVerdadeEuFalo([])
+seForVerdadeEuFalo([1, 2])
+seForVerdadeEuFalo({})
