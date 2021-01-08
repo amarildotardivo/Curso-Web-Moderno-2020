59010f224c649349920424f8989d59a62fafe8f2
amarildotardivo <58271527+amarildotardivo@users.noreply.github.com>
2021-01-08 14:23:09 -0300

    Update for1.js

--- exercicios-js/controle/for1.js
+++ exercicios-js/controle/for1.js
@@ -9,3 +9,9 @@ while(contador <= 10){
 for(let i = 1; i <= 10; i++){
     console.log(`i = ${i}`)
 }
+
+// navegando pelo array
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+for( let i = 0; i < notas.length; i++){
+    console.log(`Nota = ${notas[i]}`)
+}
