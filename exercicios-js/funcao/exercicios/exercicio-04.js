36edf1845a6b1709e78c8a9b5ded948c34473570
Amarildo Tardivo <58271527+amarildotardivo@users.noreply.github.com>
2021-01-11 19:08:47 -0200

    Create exercicio-04.js

---
+++ exercicios-js/funcao/exercicios/exercicio-04.js
@@ -0,0 +1,8 @@
+function divisao(divisor, dividendo){
+    resultado_divisao = divisor / dividendo
+    resto_divisao = divisor % dividendo
+
+    return console.log(`Resultado da divisão é: ${resultado_divisao} e o Resto é: ${resto_divisao}`)
+}
+
+divisao(8, 2)
