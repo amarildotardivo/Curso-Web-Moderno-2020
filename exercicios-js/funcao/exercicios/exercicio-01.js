17c50ce51f1974bd4fd60d7866c9e6584114adc9
Amarildo Tardivo <58271527+amarildotardivo@users.noreply.github.com>
2021-01-11 17:47:08 -0200

    Create exercicio-01.js

---
+++ exercicios-js/funcao/exercicios/exercicio-01.js
@@ -0,0 +1,10 @@
+function aritimetica(a, b){
+    soma = a + b
+    subtrair = a - b
+    multiplicar = a * b
+    dividir = a / b
+
+    console.log(`Soma: ${soma}, Subtração: ${subtrair}, Multiplicação: ${multiplicar}, Divisão: ${dividir}`)
+}
+
+aritimetica(1, 2)
