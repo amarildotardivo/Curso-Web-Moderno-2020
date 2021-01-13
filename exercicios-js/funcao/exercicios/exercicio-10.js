60e5541353021467dd30dc64208522a2a2a6f32c
Amarildo Tardivo <58271527+amarildotardivo@users.noreply.github.com>
2021-01-13 19:55:37 -0200

    Create exercicio-10.js

---
+++ exercicios-js/funcao/exercicios/exercicio-10.js
@@ -0,0 +1,16 @@
+//função que verifica se um número inteiro é divisível por 3
+/*
+function divisivel_por_3(num_inteiro){
+    return resultado = num_inteiro % 3 === 0
+}
+
+let numero = 14
+
+let result = divisivel_por_3(numero)
+console.log(`O número ${numero} é divisível por 3? ${result}`)
+*/
+
+var num_inteiro = 15
+
+let resultado = divisivel_por_3 => num_inteiro % 3 === 0
+console.log(resultado())
