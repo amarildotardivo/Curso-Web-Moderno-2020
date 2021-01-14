d23710fdd98ba513a9c7f24137399c0f5cfae6bb
Amarildo Tardivo <58271527+amarildotardivo@users.noreply.github.com>
2021-01-13 22:25:50 -0200

    Create execicio-14.js

---
+++ exercicios-js/funcao/exercicios/execicio-14.js
@@ -0,0 +1,16 @@
+let fruta = 'maçã'
+
+switch(fruta){
+    case 'maçã':
+        console.log('Não vendemos maçã')
+        break
+    case 'kiwi':
+        console.log('Estamos com escassez de Kiwi')
+        break
+    case 'melancia':
+        console.log('Aqui está, são 3 reais o kilo')
+        break
+    default:
+        console.log('Erro!')
+
+}
