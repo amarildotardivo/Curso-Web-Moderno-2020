8748585eef12d7068967950563098cdc937a2403
Amarildo Tardivo <58271527+amarildotardivo@users.noreply.github.com>
2021-01-14 21:23:14 -0200

    Update exercicio-17.js

--- exercicios-js/funcao/exercicios/exercicio-17.js
+++ exercicios-js/funcao/exercicios/exercicio-17.js
@@ -3,16 +3,20 @@ let salario = 100
 function plano_f ( plano_e , salario) {
 
     switch(plano_e){
-        case 'a': console.log((salario*10)/100)
-    break 
+        case 'a': 
+        console.log((salario*10)/100)
+        break 
 
-    case 'b': console.log((salario*15)/100)
-    break 
+        case 'b': 
+        console.log((salario*15)/100)
+        break 
 
-    case 'c': console.log((salario*20)/100)
-    break
+        case 'c': 
+        console.log((salario*20)/100)
+        break
 
-    default: console.log('plano invalido')
+        default: 
+        console.log('plano invalido')
     }
 }
 plano_f(plano_e, salario)
