00fff7ab9228e5091785d61f2bdb24f64f9f4c4b
amarildotardivo <58271527+amarildotardivo@users.noreply.github.com>
2021-01-19 00:39:00 -0300

    Create exercicio-22.js

---
+++ exercicios-js/funcao/exercicios/exercicio-22.js
@@ -0,0 +1,11 @@
+// Criar uma função para calcular o valor a ser pago de anuidade de uma associação. 
+// A função recebe comoparâmetro um inteiro que representa o mês (1 - janeiro, 2 - fevereiro...) que foi paga e o valor da anuidade. 
+// Aanuidade deve ser paga no mês de janeiro. Por mês, é cobrado 5% de juros (sob o regime de juroscompostos). 
+// O retorno deve ser o valor a ser pago para o respectivo mês escolhido.
+
+
+// juros_composto = capital * Math.pow((1 + taxa), tempo_aplicacao)
+
+function calcular_anuidade(){
+    
+}
