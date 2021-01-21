9678a568cffde60fc7ac6b8c3de4f6b3ce699167
amarildotardivo <58271527+amarildotardivo@users.noreply.github.com>
2021-01-21 02:22:34 -0300

    Update exercicio-27.js

--- exercicios-js/funcao/exercicios/exercicio-27.js
+++ exercicios-js/funcao/exercicios/exercicio-27.js
@@ -2,8 +2,6 @@
 // calcule se existe uma criança menor, caso exista se a criança menor ultrapassará a maior e emquantos anos isso acontecerá. 
 // Utilize centímetros para as unidades de medida.
 
-// Não verifica quando a criança não ultrapassa a outra!
-
 let altura1 = 1.40
 let altura2 = 1.38
 let taxa_cresc1 = 0.03
@@ -17,15 +15,21 @@ function crescimento(altura1, altura2, taxa_cresc1, taxa_cresc2){
                 altura1 = altura1 + taxa_cresc1
                 altura2 = altura2 + taxa_cresc2
                 ano++
+                if(ano > 10){
+                    return console.log(`Criança 2: ${altura2.toFixed(2)}cm, nunca Alcançará a Criança 1: ${altura1.toFixed(2)}cm`)
+                }
             }
-            return console.log(`Criança 2: ${altura2} Criança 1: ${altura1}, passou em ${ano} anos!`)
+            return console.log(`Criança 2: ${altura2.toFixed(2)}cm Criança 1: ${altura1.toFixed(2)}cm, passou em ${ano} anos!`)
         }else if(altura1 < altura2){
             for(i = 0; altura1 < altura2; i++){
                 altura1 = altura1 + taxa_cresc1
                 altura2 = altura2 + taxa_cresc2
                 ano++
+                if(ano > 10){
+                    return console.log(`Criança 1: ${altura1.toFixed(2)}cm, nunca Alcançará a Criança 2: ${altura2.toFixed(2)}cm`)
+                }
             }
-            return console.log(`Criança 2: ${altura1} Criança 1: ${altura2}, passou em ${ano} anos!`)
+            return console.log(`Criança 2: ${altura1.toFixed(2)}cm Criança 1: ${altura2.toFixed(2)}cm, passou em ${ano} anos!`)
         }
     }
     return console.log('Têm o mesmo tamanho!')
