b0edc058c278a5d3d87cd7ca2822af9dd316ec5e
amarildotardivo <58271527+amarildotardivo@users.noreply.github.com>
2021-01-22 21:34:56 -0300

    Update exercicio-29.js

--- exercicios-js/funcao/exercicios/exercicio-29.js
+++ exercicios-js/funcao/exercicios/exercicio-29.js
@@ -2,4 +2,19 @@
 // estão no intervalo [10,20] (repare que o intervalo é fechado, ou seja, inclui o 10 e o 20) e quantos deles estão fora do intervalo, 
 // escrevendo estas informações.
 
-//NÃO ENTENDI?!?!
+let vet = [7, 8, 9, 10, 11, 19, 20, 21, 22]
+
+function percorre_vetor(vet){
+    let cont_dentro = 0, cont_fora = 0
+
+    for(i = 0; i < vet.length; i++){
+        if(vet[i] >= 10 && vet[i] <= 20){
+            cont_dentro++
+        }else{
+            cont_fora++
+        }
+    }
+    console.log(`Números Dentro: ${cont_dentro}, Fora: ${cont_fora}`)
+}
+
+percorre_vetor(vet)
