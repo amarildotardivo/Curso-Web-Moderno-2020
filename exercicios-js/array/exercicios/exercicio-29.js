a30b8f14c2ad25bdc735cc65363d8b416af91c1b
amarildotardivo <58271527+amarildotardivo@users.noreply.github.com>
2021-02-03 11:31:44 -0300

    Update exercicio-29.js

--- exercicios-js/array/exercicios/exercicio-29.js
+++ exercicios-js/array/exercicios/exercicio-29.js
@@ -7,16 +7,18 @@ segundoMaior([8, 4, 5, 6]) // retornará 6
 */
 
 function segundoMaior(arrNumber){
-    let max = arrNumber.reduce(function(a, b) {
+    let max = arrNumber.reduce(function(a, b) { // Encontra o maior numero do array
         return Math.max(a, b)
       })
-    
-    for(let i = 0; i < arrNumber.lentgh; i++){
-        if(arrNumber[i] === max){
-            console.log("Deletado: " + arrNumber[i])
-            delete arrNumber[i]
-        }
-    }
+
+      let indiceE = arrNumber.indexOf(max) // Pega o indice do maior número do array
+
+      arrNumber.splice(indiceE, 1) // Exclui o maior numero do array
+
+      let segundoMax = arrNumber.reduce(function(a, b) { // Encontra o maior numero do array novamente
+        return Math.max(a, b)
+      })
+      return console.log("Segundo Maior: " + segundoMax)
 }
 
 segundoMaior([12, 16, 1, 5]) // retornará 12
