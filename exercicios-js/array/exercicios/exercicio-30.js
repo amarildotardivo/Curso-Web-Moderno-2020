b57f98ebbe9da5683c67e7546a2dda951b5fb9eb
amarildotardivo <58271527+amarildotardivo@users.noreply.github.com>
2021-02-03 13:12:32 -0300

    Update exercicio-30.js

--- exercicios-js/array/exercicios/exercicio-30.js
+++ exercicios-js/array/exercicios/exercicio-30.js
@@ -11,17 +11,27 @@ Carla: [7, 7, 8, 9] // média 7.75
 }) // retornará { nome: "Mariana", media: 7.875 }
 */
 
+// Função que realiza soma de todos elementos de um array simples
 const soma = array => array.reduce((acumulador, atual) => acumulador + atual, 0)
+
+// Função que realiza a média de um array simples, chamando a função soma
 const media = array => soma(array) / array.length
+
 function recerberMelhorEstudante(estudantes) {
-const estudantesComMedias = Object.entries(estudantes).map( estudante => {
-const chave = 0,
-valor = 1
-return { nome: estudante[chave], media: media(estudante[valor]) }
-})
-const estudantesOrdenados = estudantesComMedias.sort( (estudanteA, estudanteB) => estudanteB.media - estudanteA.media )
-const melhorEstudante = estudantesOrdenados[0]
-return melhorEstudante
+    const estudantesComMedias = Object.entries(estudantes).map( estudante => {// Pega todas as entradas e passa valores dos indices
+    const chave = 0,
+    valor = 1
+    return { nome: estudante[chave], media: media(estudante[valor]) } // Acessa chave e valor, e em valor chama a função média
+    })
+
+    // Ordena o objeto usando de forma decrescente
+    const estudantesOrdenados = estudantesComMedias.sort( (estudanteA, estudanteB) => estudanteB.media - estudanteA.media )
+    
+    // Atribui o primeiro grupo de chave: valor do objeto a um novo objeto
+    const melhorEstudante = estudantesOrdenados[0]
+
+    // Retorna o valor deste objeto ao usuário
+    return melhorEstudante
 }
 
 console.log(recerberMelhorEstudante({
