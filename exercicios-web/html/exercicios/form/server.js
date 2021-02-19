1951b51d99766e83569e7fddf8d86505f43de83e
Amarildo Tardivo <amarildotardivo@users.noreply.github.com>
2021-02-19 00:42:40 -0300

    up

--- exercicios-web/html/exercicios/form/server.js
+++ exercicios-web/html/exercicios/form/server.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const { response } = require('express')
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios', (req, resp) => {
     console.log(req.body)
-    response.send('<h1>Parabéns</h1>')
+    console.log(req.query) // usado com GET, pois no mesmo passa os parametros por url
+    resp.send('<h1>Parabéns. Usuário Incluido!!!</h1>')
+})
+
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabéns. Usuário Alterado!!!</h1>')
 })
 
 app.listen(3003)
